dff0f824ca28db1f13d08c1d55fd1043dfa649a6
unknown <anishag629@gmail.com>
2024-04-03 18:38:30 +0530

    header file and server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,19 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+
+
+
 
 
 function App() {
@@ -6,7 +21,19 @@ function App() {
 
   return (
     <>
-     <h1 className='text-3xl'>APP</h1>
+     
+     <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+     
+      </Routes>
+     </Router>
     </>
   )
 }
