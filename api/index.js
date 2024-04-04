476483b45ed58ff32e2e0cbf5bb890524cc80966
unknown <anishag629@gmail.com>
2024-04-04 23:35:28 +0530

    integrated mongodb and middlewares

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,6 +12,27 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 const app =express();
+app.use(express.json());
+
+
+
 app.listen(5000,()=>{
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 5000');
 })
+
+
+app.use('/api/user',userRoutes);
+
+app.use('/api/auth',authRoutes);
+
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode ,
+        message,
+    });
+});
+
